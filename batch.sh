6441cfdfdcc4d50d303c97ad8d8aebef44d88729
thespaceinvader <pablo.schenkel@altmuehlnet.de>
2014-03-20 21:13:13 +0100

    Update batch.sh
    optiimized the script ;)

--- batch.sh
+++ batch.sh
@@ -1,15 +1,25 @@
 #!/bin/bash 
+#setup your librarypath ;)
+library="/path/to/the/lib/"
+
 echo ''
 echo ' *---------------------------------------------------------------------------*'
 echo '|      SoundcloudMusicDownloader(cURL/Wget version) |   FlyinGrub rework      |'
 echo ' *---------------------------------------------------------------------------*'
-echo 'thespaceinvader\'s mucho mas mod!'
+echo 'thespaceinvaders mucho mas mod!'
          
 while read line           
 do           
 	./scdl.sh $line
-done <batch.txt          
 
-ls *.mp3 >> list
+	echo -n '[B] Adding '
+	find ./ -type f | wc -l | echo -n
+	echo 'files to the list'
+	
+	ls *.mp3 >> list
+	
+	echo '[B] moving this tracks to the final media directory'
+	mv -v *.mp3 $library 
 
-mv *.mp3 /path/to/libraray/
+done <batch.txt           
+# The librarypath moved to the 3rd line ;)
